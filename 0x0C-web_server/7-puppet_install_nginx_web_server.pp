39199b028d0332c8d190db2a26b9fe40179c4303
Okinbird <okinyinka@yahoo.co.uk>
2023-01-12 09:56:57 -0800

    web

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,21 @@
+# Manifet to install nginx a some other additions
+
+package { 'nginx':
+  ensure => installed,
+}
+
+file_line { 'Add redirection, 301':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
