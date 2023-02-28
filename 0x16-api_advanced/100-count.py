0e8bc625ba1aaa95cd6f147b8853173953550f1c
Okinbird <okinyinka@yahoo.co.uk>
2023-02-28 14:16:24 -0800

    count word

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,40 +1,54 @@
 #!/usr/bin/python3
-"""GET to api"""
+"""Function to count words in all hot posts of a given Reddit subreddit."""
 import requests
 
 
-def recurse(subreddit, word_list, after="", dic={}):
-    """recursive function"""
-    base_url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    h = {'User-Agent': 'Reddit API test'}
-    params = {'limit': 200, 'after': after}
-    r = requests.get(base_url, headers=h, allow_redirects=False, params=params)
-    if r.status_code != 200:
-        return None
-    d = r.json()
+def count_words(subreddit, word_list, instances={}, after="", count=0):
+    """Prints counts of given words found in hot posts of a given subreddit.
+    Args:
+        subreddit (str): The subreddit to search.
+        word_list (list): The list of words to search for in post titles.
+        instances (obj): Key/value pairs of words/counts.
+        after (str): The parameter for the next page of the API results.
+        count (int): The parameter of results matched thus far.
+    """
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/okinbird)"
+    }
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    try:
+        results = response.json()
+        if response.status_code == 404:
+            raise Exception
+    except Exception:
+        print("")
+        return
+
+    results = results.get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for c in results.get("children"):
+        title = c.get("data").get("title").lower().split()
+        for word in word_list:
+            if word.lower() in title:
+                times = len([t for t in title if t == word.lower()])
+                if instances.get(word) is None:
+                    instances[word] = times
+                else:
+                    instances[word] += times
+
     if after is None:
-        return dic
-    l = d.get('data', {}).get('children')
-    for i in l:
-        title = i.get('data', {}).get('title').lower().split()
-        for j in word_list:
-            for t in title:
-                if j == t:
-                    if j not in dic:
-                        dic[j] = 1
-                    else:
-                        dic[j] += 1
-    p = d.get('data', {}).get('after')
-    return recurse(subreddit, word_list, p, dic)
-
-
-def count_words(subreddit, word_list):
-    """Cound # of keywords"""
-    for i, e in enumerate(word_list):
-        word_list[i] = e.lower()
-    d = recurse(subreddit, word_list)
-    if d:
-        for key, value in sorted(d.items(), key=lambda x: (-x[1], x[0])):
-            print("{}: {}".format(key, value))
-    elif d is None:
-        print()
+        if len(instances) == 0:
+            print("")
+            return
+        instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
+        [print("{}: {}".format(k, v)) for k, v in instances]
+    else:
+        count_words(subreddit, word_list, instances, after, count)
