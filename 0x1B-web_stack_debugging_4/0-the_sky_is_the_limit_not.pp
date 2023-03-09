38d925561020a22e9ee6c2f9631929f8055a439f
Okinbird <okinyinka@yahoo.co.uk>
2023-03-09 07:39:23 -0800

    fix limit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # Increase limit of Nginx server traffic request
 exec { 'Request':
-  command => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 5000\"/g" /etc/default/nginx',
   path => '/usr/bin:/usr/sbin:/bin',
+  command => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 5000\"/g" /etc/default/nginx',
 }
 -> exec { 'restart service':
   command => '/usr/sbin/service nginx restart',
