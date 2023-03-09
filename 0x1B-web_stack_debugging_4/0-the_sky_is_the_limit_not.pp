72c9b184b0eab00ff9980791b9cf110ddbda9675
Okinbird <okinyinka@yahoo.co.uk>
2023-03-09 07:32:35 -0800

    user limit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # Increase limit of Nginx server traffic request
 exec { 'Request':
-  path    => ['/bin'],
-  command => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 5000\"/" /etc/default/nginx',
+  path    => '/usr/bin:/usr/sbin:/bin',
+  command => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 5000\"/g" /etc/default/nginx',
   notify  => Service['nginx'],
 }
 service { 'Nginx':
--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,12 +1,12 @@
 # Changes user limit
 exec { 'increase hard limit':
-  path    => ['/bin'],
-  command => 'sed -i "s/holberton hard nofile 5/holberton hard nofile 1024/" /etc/security/limits.conf',
+  path    => '/usr/bin:/usr/sbin:/bin',
+  command => 'sed -i "s/holberton hard nofile 5/holberton hard nofile 5000/" /etc/security/limits.conf',
 }
 
 exec { 'increase soft limit':
-  path    => ['/bin'],
-  command => 'sed -i "s/holberton soft nofile 4/holberton soft nofile 1024/" /etc/security/limits.conf',
+  path    => '/usr/bin:/usr/sbin:/bin',
+  command => 'sed -i "s/holberton soft nofile 4/holberton soft nofile 4000/" /etc/security/limits.conf',
 }
 -> exwc { 'refresh conf':
   command => '/sbin/sysdctl -p',
